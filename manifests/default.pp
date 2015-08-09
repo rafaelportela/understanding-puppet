ddad9e14fbe94a3e7e0b56ac58a2487b3f5f7100
Rafael Portela <rafaelportela90@gmail.com>
2015-08-09 16:21:41 -0300

    Add Nginx config files.
    Before making sure nginx service is running, it adds the myapp conf file
    and creates a simlink to make it available. Also, the defalt site config
    is removed.

--- manifests/default.pp
+++ manifests/default.pp
@@ -24,6 +24,28 @@ node 'web.example.com' {
 
   service { 'nginx':
     ensure => running,
-    require => Package['nginx'],
+    require => [
+      Package['nginx'],
+      File['/etc/nginx/sites-enabled/myapp'],
+      File['remove default site']
+    ],
+  }
+
+  file { 'myapp.conf nginx file':
+    path => '/etc/nginx/sites-available/myapp',
+    source => 'file:///vagrant/files/nginx-myapp.conf',
+    owner => 'root',
+    group => 'root',
+  }
+
+  file { '/etc/nginx/sites-enabled/myapp':
+    ensure => 'link',
+    target => '/etc/nginx/sites-available/myapp',
+    require => File['myapp.conf nginx file'],
+  }
+
+  file { 'remove default site':
+    path => '/etc/nginx/sites-enabled/default',
+    ensure => 'absent',
   }
 }
