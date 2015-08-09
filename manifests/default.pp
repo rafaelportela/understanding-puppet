4a2aa72dddaa19430142dba12e1b402a0f7ffe57
Rafael Portela <rafaelportela90@gmail.com>
2015-08-09 13:14:59 -0300

    Install nginx package in web node.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,11 +1,24 @@
-exec { "say hello":
- command => "/bin/echo hello"
+node default {
+  exec { "say hello":
+    command => "/bin/echo hello"
+  }
+
+  host { 'web.example.com':
+    ip => '192.168.100.2',
+  }
+
+  host { 'db.example.com':
+    ip => '192.168.100.3',
+  }
 }
 
-host { 'web.example.com':
-  ip => '192.168.100.2'
-}
+node 'web.example.com' {
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
+  }
 
-host { 'db.example.com':
-  ip => '192.168.100.3'
+  package { 'nginx':
+    ensure => installed,
+    require => Exec['apt-get update']
+  }
 }
