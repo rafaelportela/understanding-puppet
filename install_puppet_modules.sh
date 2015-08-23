6ba93287d95db9fb478a0142a52c0f69e7bdf5a3
Rafael Portela <rafaelportela90@gmail.com>
2015-08-22 21:10:48 -0300

    Fix if statement on bash script to install.

--- install_puppet_modules.sh
+++ install_puppet_modules.sh
@@ -1,10 +1,10 @@
 mkdir -p /etc/puppet/modules
 sudo apt-get update
 
-$MYSQL_INSTALLED = puppet module list | grep mysql
+MYSQL_INSTALLED=`puppet module list | grep mysql`
 
 # if MYSQL_INSTALLED == '' or empty
-if [ -n "$MYSQL_INSTALLED" ]
+if [ -z "$MYSQL_INSTALLED" ]
 then
   echo "Installing puppetlabs/mysql module"
   puppet module install puppetlabs/mysql
